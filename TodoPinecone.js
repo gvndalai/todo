aac3d3b2813245c620e12bb32131af603f8212a1
Gundalai Dawgasuren <23LP0094@G304-CC0042.local>
2023-11-30 19:32:10 +0800

    nove30

--- TodoPinecone.js
+++ TodoPinecone.js
@@ -13,28 +13,29 @@ function BackdropHiding() {
     backDrop.style.display = 'none';
 }
 // number of tasks
-let todoTasksNumber = document.getElementById("To-do-card").querySelector("h3").querySelector("span").innerText
-let inProgressTasksNumber = document.getElementById("In-progress-card").querySelector("h3").querySelector("span").innerText 
-let stuckTasksNumber = document.getElementById("Stuck-card").querySelector("h3").querySelector("span").innerText 
-let doneTasksNumber = document.getElementById("Done-card").querySelector("h3").querySelector("span").innerText 
+let todoTasksNumber = 0;
+let inProgressTasksNumber = 0;
+let stuckTasksNumber = 0;
+let doneTasksNumber = 0;
 
 backDropDotorh.addEventListener("click", BackdropHiding)
 // Adding task
 const tasks = [];
 const addTaskButtonRef = document.getElementById("Adding-task-button");
-// title error
+
+// Create title error alert
 const fillTitleField = document.createElement("div");
 fillTitleField.setAttribute("id", "title-alert");
 fillTitleField.textContent = "Please type a title";
-fillTitleField.style.display = "none"
-document.getElementById("New-task-title").appendChild(fillTitleField)
-// description error 
+fillTitleField.style.display = "none";
+document.getElementById("New-task-title").appendChild(fillTitleField);
+
+// Create description error alert
 const fillFieldDescription = document.createElement("div");
 fillFieldDescription.setAttribute("id", "description-alert");
 fillFieldDescription.textContent = "Please type a description";
+fillFieldDescription.style.display = "none";
 document.getElementById("New-task-description").appendChild(fillFieldDescription);
-fillFieldDescription.style.display = "none"
-// nondisplaying alert 
 
 function addingTask() {
     const newTaskTitleInput = document.getElementById('New-task-title-input');
@@ -48,6 +49,7 @@ function addingTask() {
 
     // NEW TASK OBJECT
     const newTaskItem = { title: newTaskTitle, description: newTaskDescription, status: statusValue, priority: newTaskPriority };
+    
     // CREATING
     const taskItem = document.createElement('li');
     taskItem.setAttribute('id', 'task-item');
@@ -89,7 +91,6 @@ function addingTask() {
     
     actions.appendChild(removeButtonRef);
 
-
     // Edit button
     const editButtonRef = document.createElement('button');
     editButtonRef.setAttribute('id', 'edit-button');
@@ -97,6 +98,8 @@ function addingTask() {
         '<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M471.6 21.7c-21.9-21.9-57.3-21.9-79.2 0L362.3 51.7l97.9 97.9 30.1-30.1c21.9-21.9 21.9-57.3 0-79.2L471.6 21.7zm-299.2 220c-6.1 6.1-10.8 13.6-13.5 21.9l-29.6 88.8c-2.9 8.6-.6 18.1 5.8 24.6s15.9 8.7 24.6 5.8l88.8-29.6c8.2-2.7 15.7-7.4 21.9-13.5L437.7 172.3 339.7 74.3 172.4 241.7zM96 64C43 64 0 107 0 160V416c0 53 43 96 96 96H352c53 0 96-43 96-96V320c0-17.7-14.3-32-32-32s-32 14.3-32 32v96c0 17.7-14.3 32-32 32H96c-17.7 0-32-14.3-32-32V160c0-17.7 14.3-32 32-32h96c17.7 0 32-14.3 32-32s-14.3-32-32-32H96z"/></svg>';
     actions.appendChild(editButtonRef);
     taskItem.appendChild(actions);
+    
+    // Appending
     if (newTaskTitle !== '' && newTaskDescription !== '') {
         if (newTaskItem.status === "Todo") {
             const toDoTasks = document.getElementById("To-do-tasks");
@@ -104,63 +107,148 @@ function addingTask() {
             newTaskTitleInput.value = '';
             newTaskDescriptionInput.value = '';
             backDrop.style.display = 'none';
-            const todoTaskCount = toDoTasks.getElementsByTagName('li').length;
-            todoTasksNumber = `Number of tasks: ${todoTaskCount}`;
+            todoTasksNumber++;
+            updateTaskCount("To-do-card", todoTasksNumber);
         } else if (newTaskItem.status === "In-progress") {
             const inProgressTasks = document.getElementById("In-progress-tasks");
             inProgressTasks.appendChild(taskItem);
             newTaskTitleInput.value = '';
             newTaskDescriptionInput.value = '';
             backDrop.style.display = 'none';
-            const inProgressTaskCount = inProgressTasks.getElementsByTagName('li').length;
-            inProgressTasksNumber = `Number of tasks: ${inProgressTaskCount}`;
+            inProgressTasksNumber++;
+            updateTaskCount("In-progress", inProgressTasksNumber);
         } else if (newTaskItem.status === "Stuck") {
             const stuckTasks = document.getElementById("Stuck-tasks");
             stuckTasks.appendChild(taskItem);
             newTaskTitleInput.value = '';
             newTaskDescriptionInput.value = '';
             backDrop.style.display = 'none';
-            const stuckTaskCount = stuckTasks.getElementsByTagName('li').length;
-            stuckTasksNumber = `Number of tasks: ${stuckTaskCount}`;
+            stuckTasksNumber++;
+            updateTaskCount("Stuck-card", stuckTasksNumber);
         } else if (newTaskItem.status === "Done") {
             const doneTasks = document.getElementById("Done-tasks");
             doneTasks.appendChild(taskItem);
             newTaskTitleInput.value = '';
             newTaskDescriptionInput.value = '';
             backDrop.style.display = 'none';
-            const doneTaskCount = doneTasks.getElementsByTagName('li').length;
-            doneTasksNumber = `Number of tasks: ${doneTaskCount}`;
-            
+            doneTasksNumber++;
+            updateTaskCount("Done-card", doneTasksNumber);
         }
     }
+    function updateTaskCount(cardId, count) {
+        const countElement = document.getElementById(cardId).querySelector("h3").querySelector("span");
+        if (countElement) {
+            countElement.textContent = count;
+        }
+    }
+    // Validation for empty fields
     if (newTaskTitle === '' || newTaskDescription === '') {
         if (newTaskTitle === '') {
             document.getElementById("New-task-title-input").style.borderColor = "red";
-            fillTitleField.style.display = "block"
+            fillTitleField.style.display = "block";
         }
         if (newTaskDescription === '') {
             document.getElementById("New-task-description-input").style.borderColor = "red";
-            fillFieldDescription.style.display = "block"
+            fillFieldDescription.style.display = "block";
         }
-        return; 
+        return;
     }
-    if( backDrop.style.display = 'none'){
-        fillFieldDescription.style.display = "none";
-        document.getElementById("New-task-title-input").style.borderColor = "black";
-         document.getElementById("New-task-description-input").style.borderColor = "black";
-        fillTitleField.style.display = "none"
-    }
+
+    // Hide error alerts and reset border colors
+    fillTitleField.style.display = "none";
+    fillFieldDescription.style.display = "none";
+    document.getElementById("New-task-title-input").style.borderColor = "black";
+    document.getElementById("New-task-description-input").style.borderColor = "black";
+
+    // Remove button click event
     removeButtonRef.addEventListener("click", function () {
         const taskItem = removeButtonRef.closest('#task-item');
         if (taskItem) {
             taskItem.parentNode.removeChild(taskItem);
-        
+        }
+        if (newTaskItem.status === "In-progress") {
+            inProgressTasksNumber--;
+            updateTaskCount("In-progress-card", inProgressTasksNumber);
+        } else if (newTaskItem.status === "Done") {
+            doneTasksNumber--;
+            updateTaskCount("Done-card", doneTasksNumber);
+        } else if (newTaskItem.status === "Stuck") {
+            stuckTasksNumber--;
+            updateTaskCount("Stuck-card", stuckTasksNumber);
+        } else if (newTaskItem.status === "Todo") {
+            todoTasksNumber--;
+            updateTaskCount("To-do-card", todoTasksNumber);
         }
     });
+    
+
+    // Check button click event
+    checkButtonRef.onclick = function () {
+        console.log("function processing")
+        newTaskItem.status === "Done"
+            const doneTasks = document.getElementById("Done-tasks");
+            doneTasks.appendChild(taskItem);
+            newTaskTitleInput.value = '';
+            newTaskDescriptionInput.value = '';
+            backDrop.style.display = 'none';
+    
+            // if (taskItem) {
+            //     taskItem.parentNode.removeChild(taskItem);
+            // }
+            // if (newTaskItem.status === "In-progress") {
+            //     inProgressTasksNumber--;
+            //     updateTaskCount("In-progress-card", inProgressTasksNumber);
+            // } else if (newTaskItem.status === "Done") {
+            //     doneTasksNumber--;
+            //     updateTaskCount("Done-card", doneTasksNumber);
+            // } else if (newTaskItem.status === "Stuck") {
+            //     stuckTasksNumber--;
+            //     updateTaskCount("Stuck-card", stuckTasksNumber);
+            // } else if (newTaskItem.status === "Todo") {
+            //     todoTasksNumber--;
+            //     updateTaskCount("To-do-card", todoTasksNumber);
+            // }   
+    };
+
+    // Edit button click event
+    editButtonRef.onclick = function () {
+        backDrop.style.display = 'block';
+        newTaskTitleInput.value = newTaskItem.title;
+        newTaskDescriptionInput.value = newTaskItem.description;
+        newTaskPriorityInput.value = newTaskPriority;
+        selectedStatus.value = statusValue;
+
+        // Task editing
+        if (newTaskTitle !== '' && newTaskDescription !== '') {
+            newTaskItem.title = newTaskTitle;
+            newTaskItem.description = newTaskDescription;
+            newTaskItem.priority = newTaskPriority;
+            newTaskItem.status = statusValue;
+
+            taskTitle.innerText = newTaskTitle;
+            textNote.innerText = newTaskDescription;
+            priority.innerText = newTaskPriority;
+        }
+
+        // Reset the "Add Task" button functionality
+        addTaskButtonRef.onclick = function () {
+            backDrop.style.display = 'none';
+            addingTask();
+            if (taskItem) {
+                taskItem.parentNode.removeChild(taskItem);
+            }
+        };
+    };
 }
 
-
+// Add event listener for "Add Task" button
 addTaskButtonRef.addEventListener("click", addingTask);
+
+
+
+
+
+
 
 
 
